b3b0dc14959c1b33895599c738a5a9b478d5f83e
Carolyn Lam <caro_lyn_@hotmail.com>
2017-02-03 16:53:45 -0500

    create HashTable and HashNode constructors

---
+++ hash_table.js
@@ -0,0 +1,12 @@
+function HashTable(size) {
+  // determines how big the hash table is
+  // how many buckets we have
+  this.buckets = Array(size);
+  this.numBuckets = this.buckets.length;
+}
+
+function HashNode(key, value, next) {
+  this.key = key;
+  this.value = value;
+  this.next = next || null;
+}
